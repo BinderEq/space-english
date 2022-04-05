b1ac04dc8d495bf4c2bbfb6fc385ea6e205741c2
OrthodoxEgo <vgtrofimov@gmail.com>
2022-04-05 18:23:46 +0300

    Создал базу главного файла.

---
+++ space-english.py
@@ -0,0 +1,40 @@
+# Подключаем pygame
+import pygame
+
+pygame.init()
+size = [WIDTH, HEIGHT]
+pygame.display.set_caption("Шаблон pygame")
+scene = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+# ГЛАВНЫЙ ЦИКЛ ИГРЫ
+while (playGame):
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            playGame = False
+        elif (event.type == pygame.KEYDOWN):
+            if (event.key == pygame.K_ESCAPE):
+                playGame = False
+            elif (event.key == pygame.K_LEFT):
+                print("ВЛЕВО")
+            elif (event.key == pygame.K_RIGHT):
+                print("ВПРАВО")
+            elif (event.key == pygame.K_UP):
+                print("ВВЕРХ")
+            elif (event.key == pygame.K_DOWN):
+                print("ВНИЗ")
+
+    # Очищаем сцену
+    scene.fill(BLACK)
+
+    # Отрисовываем изображения
+    pygame.display.flip()
+
+    # Расчёты:
+    # ...
+
+    # Задержка для синхронизации FPS
+    clock.tick(FPS)
+
+# Конец истории
+pygame.quit()
